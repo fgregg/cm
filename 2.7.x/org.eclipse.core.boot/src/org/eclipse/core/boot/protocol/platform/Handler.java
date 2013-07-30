bc9862e7cf6e10d999528a3991e3ec3a05efcb5b
rphall <rick@rphall.com>
2013-07-30 17:29:13 -0400

    Initial version

---
+++ 2.7.x/org.eclipse.core.boot/src/org/eclipse/core/boot/protocol/platform/Handler.java
@@ -0,0 +1,23 @@
+/**
+ * @(#)$RCSfile: Handler.java,v $  $Revision: 1.1 $ $Date: 2003/05/09 00:02:26 $
+ * 
+ * Copyright (c) 2003 ChoiceMaker Technologies, Inc. 
+ * 41 East 11th Street, New York, NY 10003 
+ * All rights reserved.
+ * 
+ * This software is the confidential and proprietary information of 
+ * ChoiceMaker Technologies Inc. ("Confidential Information"). 
+ */
+
+package org.eclipse.core.boot.protocol.platform;
+
+import org.eclipse.core.internal.boot.PlatformURLHandler;
+
+/**
+ * Comment
+ *
+ * @author   Martin Buechi
+ * @version  $Revision: 1.1 $ $Date: 2003/05/09 00:02:26 $
+ */
+public class Handler extends PlatformURLHandler {
+}
