dbd512fd29509c5517f8899ba535635bef59c05d
rphall <rick@rphall.com>
2013-08-13 07:42:52 -0400

    Added ChoiceMaker Blocking Plugin (Base)

---
+++ 2.7.x/com.choicemaker.cm.io.blocking.base/src/com/choicemaker/cm/io/blocking/base/gen/BlockingTags.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2001, 2009 ChoiceMaker Technologies, Inc. and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Eclipse Public License
+ * v1.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     ChoiceMaker Technologies, Inc. - initial API and implementation
+ */
+package com.choicemaker.cm.io.blocking.base.gen;
+
+public class BlockingTags {
+	public static final String BLOCKING = "blocking";
+	public static final String DEFAULT_COUNT = "defaultCount";
+	public static final String BLOCKING_FIELD = "blockingField";
+	public static final String SOURCE_GROUP = "sourceGroup";
+	public static final String TARGET_GROUP = "targetGroup";
+	public static final String BASE = "base";
+	public static final String KEY = "key";
+	public static final String ILLEGAL_COMBINATION = "illegalCombination";
+	public static final String SOURCE_FIELD_REF = "sourceFieldRef";
+	public static final String BLOCKING_FIELD_REF = "blockingFieldRef";
+	public static final String TARGET_FIELD_REF = "targetFieldRef";
+	public static final String SOURCE_FIELD_NAME = "sourceFieldName";
+	public static final String SOURCE_NODE_TYPE_NAME = "sourceNodeTypeName";
+	public static final String TARGET_FIELD_NAME = "targetFieldName";
+	public static final String TARGET_NODE_TYPE_NAME = "targetNodeTypeName";	
+	public static final String DB_CONF = "dbConf";
+	public static final String EXACT = "exact";
+	public static final String AUTOMATED = "automated";
+	public static final String SOURCE_VALID = "sourceValid";
+	public static final String TARGET_VALID = "targetValid";
+}
