e9e7369343ddf822985a0f5e43d5e564a6aaed53
rphall <rick@rphall.com>
2013-08-16 12:21:07 -0400

    Added ChoiceMaker Maximum Entropy Plugin (GUI)

---
+++ 2.7.x/com.choicemaker.cm.ml.me.gui/src/com/choicemaker/cm/ml/me/gui/MeGuiFactory.java
@@ -0,0 +1,146 @@
+/*
+ * Copyright (c) 2001, 2009 ChoiceMaker Technologies, Inc. and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Eclipse Public License
+ * v1.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     ChoiceMaker Technologies, Inc. - initial API and implementation
+ */
+package com.choicemaker.cm.ml.me.gui;
+
+import javax.swing.*;
+import javax.swing.table.*;
+
+import com.choicemaker.cm.core.ml.MachineLearner;
+import com.choicemaker.cm.core.util.MessageUtil;
+import com.choicemaker.cm.ml.me.base.MaximumEntropy;
+import com.choicemaker.cm.modelmaker.gui.hooks.TrainDialogPlugin;
+import com.choicemaker.cm.modelmaker.gui.ml.MlGuiFactory;
+import com.choicemaker.cm.modelmaker.gui.tables.*;
+import com.choicemaker.cm.modelmaker.gui.utils.NullFloat;
+
+/**
+ * Description
+ * 
+ * @author  Martin Buechi
+ * @version $Revision: 1.1.1.1 $ $Date: 2009/05/03 16:03:06 $
+ */
+public class MeGuiFactory extends MlGuiFactory {
+	private ClueTableModelPlugin clueTableModelPlugin;
+	private ActiveClueTableModelPlugin activeClueTableModelPlugin;
+
+	public ClueTableModelPlugin getClueTableModelPlugin() {
+		if (clueTableModelPlugin == null) {
+			clueTableModelPlugin = new ClueTableModelPlugin() {
+				TableColumn weightColumn;
+				public TableColumn getColumn(int column) {
+					if (weightColumn == null) {
+						DefaultTableCellRenderer renderer = new DefaultTableCellRenderer();
+						renderer.setHorizontalAlignment(JLabel.RIGHT);
+						TableCellEditor editor = new DefaultCellEditor(new JTextField());
+						weightColumn = new TableColumn(startColumn, 150, renderer, editor);
+					}
+					return weightColumn;
+				}
+				public String getColumnName(int column) {
+					return MessageUtil.m.formatMessage("train.gui.modelmaker.table.common.weight");
+				}
+				public boolean isCellEditable(int row, int column) {
+					return !model.getClueSet().getClueDesc()[row].rule;
+				}
+				public Object getValueAt(int row, int column) {
+					float[] weights = ((MaximumEntropy) model.getMachineLearner()).getWeights();
+					return weights == null
+						|| model.getClueSet().getClueDesc()[row].rule
+							? NullFloat.getNullInstance()
+							: new NullFloat(weights[row]);
+				}
+				public void setValueAt(Object value, int row, int col) {
+					try {
+						float we = Float.parseFloat(value.toString());
+						((MaximumEntropy) model.getMachineLearner()).getWeights()[row] = we;
+					} catch (NumberFormatException ex) {
+						// ignore
+					}
+				}
+				public int getColumnCount() {
+					return 1;
+				}
+				public int getRowCount() {
+					throw new UnsupportedOperationException();
+				}
+			};
+		}
+		return clueTableModelPlugin;
+	}
+
+	public ActiveClueTableModelPlugin getActiveClueTableModelPlugin() {
+		if (activeClueTableModelPlugin == null) {
+			activeClueTableModelPlugin = new ActiveClueTableModelPlugin() {
+				TableColumn weightColumn;
+				public TableColumn getColumn(int column) {
+					if (weightColumn == null) {
+						DefaultTableCellRenderer renderer = new DefaultTableCellRenderer();
+						renderer.setHorizontalAlignment(JLabel.RIGHT);
+						TableCellEditor editor = new DefaultCellEditor(new JTextField());
+						weightColumn = new TableColumn(startColumn, 150, renderer, editor);
+					}
+					return weightColumn;
+				}
+				public String getColumnName(int column) {
+					return MessageUtil.m.formatMessage("train.gui.modelmaker.table.common.weight");
+				}
+				public boolean isCellEditable(int row, int column) {
+					return false;
+				}
+				public Object getValueAt(int row, int column) {
+					if (model.getClueSet().getClueDesc()[row].rule) {
+						return NullFloat.getNullInstance();
+					} else {
+						return new NullFloat(((MaximumEntropy) model.getMachineLearner()).getWeights()[row]);
+					}
+				}
+				public void setValueAt(Object value, int row, int col) {
+					throw new UnsupportedOperationException();
+				}
+				public int getColumnCount() {
+					return 1;
+				}
+				public int getRowCount() {
+					throw new UnsupportedOperationException();
+				}
+			};
+		}
+		return activeClueTableModelPlugin;
+	}
+	/**
+	 * @see com.choicemaker.cm.ml.gui.MlGuiFactory#getTrainDialogPlugin(com.choicemaker.cm.ml.MachineLearner)
+	 */
+	public TrainDialogPlugin getTrainDialogPlugin(MachineLearner learner) {
+		return new MeTrainDialogPlugin((MaximumEntropy) learner);
+	}
+	/**
+	 * @see com.choicemaker.cm.core.DynamicDispatchHandler#getHandler()
+	 */
+	public Object getHandler() {
+		return this;
+	}
+	/**
+	 * @see com.choicemaker.cm.core.DynamicDispatchHandler#getHandledType()
+	 */
+	public Class getHandledType() {
+		return MaximumEntropy.class;
+	}
+
+	public String toString() {
+		return MessageUtil.m.formatMessage("ml.me.label");
+	}
+	/**
+	 * @see com.choicemaker.cm.ml.gui.MlGuiFactory#getMlInstance()
+	 */
+	public MachineLearner getMlInstance() {
+		return new MaximumEntropy();
+	}
+}
---
+++ 2.7.x/com.choicemaker.cm.ml.me.gui/src/com/choicemaker/cm/ml/me/gui/MeTrainDialogPlugin.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2001, 2009 ChoiceMaker Technologies, Inc. and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Eclipse Public License
+ * v1.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     ChoiceMaker Technologies, Inc. - initial API and implementation
+ */
+package com.choicemaker.cm.ml.me.gui;
+
+import java.awt.FlowLayout;
+
+import javax.swing.*;
+
+import com.choicemaker.cm.core.util.MessageUtil;
+import com.choicemaker.cm.ml.me.base.MaximumEntropy;
+import com.choicemaker.cm.modelmaker.gui.dialogs.TrainDialog;
+import com.choicemaker.cm.modelmaker.gui.hooks.TrainDialogPlugin;
+import com.choicemaker.cm.modelmaker.gui.utils.EnablednessGuard;
+
+/**
+ *
+ * @author    
+ * @version   $Revision: 1.1.1.1 $ $Date: 2009/05/03 16:03:06 $
+ */
+public class MeTrainDialogPlugin extends TrainDialogPlugin {
+	private JLabel trainingIterationsLabel;
+	private JTextField trainingIterations;
+	private MaximumEntropy me;
+	private TrainDialog trainDialog;
+
+	public MeTrainDialogPlugin(MaximumEntropy me) {
+		this.me = me;
+		setBorder(BorderFactory.createTitledBorder(MessageUtil.m.formatMessage("ml.me.train.label")));
+		trainingIterationsLabel = new JLabel(MessageUtil.m.formatMessage("ml.me.train.iterations"));
+		trainingIterations = new JTextField(5);
+		trainingIterations.setText(String.valueOf(me.getTrainingIterations()));
+		setLayout(new FlowLayout(FlowLayout.LEFT));
+		add(trainingIterationsLabel);
+		add(trainingIterations);
+	}
+	
+	public void setTrainDialog(TrainDialog trainDialog) {
+		this.trainDialog = trainDialog;
+		EnablednessGuard dl = new EnablednessGuard(trainDialog);
+		trainingIterations.getDocument().addDocumentListener(dl);
+	}
+	
+	public boolean isParametersValid() {
+		try {
+			return Integer.parseInt(trainingIterations.getText()) > 0;
+		} catch (NumberFormatException ex) {
+			// ignore
+		}
+		return false;
+	}
+
+	public void set() {
+		me.setTrainingIterations(Integer.parseInt(trainingIterations.getText()));
+	}
+}
