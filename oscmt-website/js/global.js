ad1bd588abea84a682e667389ee7e6d6949e96ee
rphall <rick@rphall.com>
2013-12-26 08:44:41 -0500

    Removed stuff

--- oscmt-website/js/global.js
+++
@@ -1,9 +0,0 @@
-var quickSearchDefaultValue = 'Search';
-
-function resetInput (theInput) {
-	if (theInput.value == quickSearchDefaultValue) theInput.value = '';
-}
-
-function getObj (objID) {
-	return document.getElementById (objID);
-}
