6e670c25d57a11e864895c943fc37d37b0c30591
rphall <rick@rphall.com>
2014-04-17 09:42:17 -0400

    Generated by org.apache.maven.archetypes:maven-archetype-plugin:1.2:generate

---
+++ 2.7.x/cluemaker-maven-plugin/src/test/java/maven-plugin/MyMojoTest.java
@@ -0,0 +1,39 @@
+package com.choicemaker.maven;
+
+import java.io.File;
+import org.apache.maven.plugin.testing.AbstractMojoTestCase;
+
+public class MyMojoTest
+    extends AbstractMojoTestCase
+{
+    /** {@inheritDoc} */
+    protected void setUp()
+        throws Exception
+    {
+        // required
+        super.setUp();
+    }
+
+    /** {@inheritDoc} */
+    protected void tearDown()
+        throws Exception
+    {
+        // required
+        super.tearDown();
+    }
+
+    /**
+     * @throws Exception if any
+     */
+    public void testSomething()
+        throws Exception
+    {
+        File pom = getTestFile( "src/test/resources/unit/mymojo/test-pom.xml" );
+        assertNotNull( pom );
+        assertTrue( pom.exists() );
+
+        MyMojo myMojo = (MyMojo) lookupMojo( "touch", pom );
+        assertNotNull( myMojo );
+        myMojo.execute();
+    }
+}
