607af567975653e7193775d0d42a43071713be5d
Rick Hall <rick@rphall.com>
2014-07-25 10:50:26 -0400

    Working version of Arquillian Getting Started Guide

---
+++ 2.7.x/misc/cm-server-ejb/src/main/java/com/choicemaker/fake/PluginDiscoveryBean.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.choicemaker.fake;
+
+import java.util.LinkedList;
+import java.util.List;
+
+//import javax.ejb.LocalBean;
+//import javax.ejb.Stateless;
+
+/**
+ * Bean that uses PluginDiscovery
+ * rphall
+ */
+
+
+public class PluginDiscoveryBean {
+
+    public List<String> listPluginIds() {
+    	// FIXME hard-coded, fake list
+    	List<String> retVal = new LinkedList<>();
+    	retVal.add("com.choicemaker.fake.plugin01");
+    	return retVal;
+    }
+
+}
