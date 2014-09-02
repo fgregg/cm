4bd9d322310f2fdbbe70342703d6f37e2403daf5
Rick Hall <rick@rphall.com>
2014-09-02 08:03:54 -0400

    Removed misc/com.choicemaker.cm.core2

--- 2.7.x/misc/com.choicemaker.cm.core2/src/main/java/com/choicemaker/cm/core/configure/ProbabilityModelConfiguration.java
+++
@@ -1,5 +0,0 @@
-package com.choicemaker.cm.core.configure;
-
-public interface ProbabilityModelConfiguration {
-
-}
--- 2.7.x/misc/com.choicemaker.cm.core2/src/main/java/com/choicemaker/cm/core/gen/IGeneratorPluginFactory.java
+++
@@ -1,8 +0,0 @@
-package com.choicemaker.cm.core.gen;
-
-import java.util.List;
-
-public interface IGeneratorPluginFactory {
-	/** Look up generator plugins */
-	List lookupGeneratorPlugins() throws GenException;
-}
