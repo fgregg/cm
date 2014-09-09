0c2cd4518d8e2f82d2fd1f13c78c08ba69bbd778
Rick Hall <rick@rphall.com>
2014-09-08 23:34:59 -0400

    simple_person_matching: Checkpt: model compiles under Maven

---
+++ 2.7.x/choicemaker-cm/model_projects/simple_person_matching/src/main/eclipse2/plugin/etc/traindata/generic_person/rs/flatfile-01.rs
@@ -0,0 +1,2 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<RecordSource class="com.choicemaker.cm.io.flatfile.base.flatfileRsReader"><fileNamePrefix>flatfile-01</fileNamePrefix><fileNameSuffix>.txt</fileNameSuffix><multiFile>false</multiFile><singleLine>false</singleLine><fixedLength>false</fixedLength><separatorChar>|</separatorChar><tagged>true</tagged></RecordSource>
