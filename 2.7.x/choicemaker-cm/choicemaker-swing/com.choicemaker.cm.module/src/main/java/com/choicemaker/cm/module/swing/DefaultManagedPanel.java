68712adfebf570b8cd0b89e407490af72506b3f4
Rick Hall <rick@rphall.com>
2014-09-23 19:23:39 -0400

    cm.module: Fixed a plugin lookup bug in DefaultManagedPanel

--- 2.7.x/choicemaker-cm/choicemaker-swing/com.choicemaker.cm.module/src/main/java/com/choicemaker/cm/module/swing/DefaultManagedPanel.java
+++ 2.7.x/choicemaker-cm/choicemaker-swing/com.choicemaker.cm.module/src/main/java/com/choicemaker/cm/module/swing/DefaultManagedPanel.java
@@ -18,6 +18,7 @@ import com.choicemaker.cm.module.IModuleController;
 import com.choicemaker.cm.module.IModuleController.IUserInterface;
 import com.choicemaker.e2.CMConfigurationElement;
 import com.choicemaker.e2.CMExtension;
+import com.choicemaker.e2.CMExtensionPoint;
 import com.choicemaker.e2.platform.CMPlatformUtils;
 
 /**
@@ -53,10 +54,11 @@ public class DefaultManagedPanel extends AbstractTabbedPanel implements IModelMa
 	}
 
 	/**
-	 * Create an instance of an {@link #IModule} configuration from
-	 * the PluginRegistry. The configuration must be unique within the registry.
-	 * If no configuration exists within the register, an instance of
+	 * Create an instance of an {@link #IModule} configuration from the
+	 * PluginRegistry. The configuration must be unique within the registry. If
+	 * no configuration exists within the register, an instance of
 	 * {@link #DefaultModule} is returned
+	 * 
 	 * @return a non-null instance of a IModule configuration.
 	 */
 	public IModuleController createModule(Document d) {
@@ -65,37 +67,41 @@ public class DefaultManagedPanel extends AbstractTabbedPanel implements IModelMa
 		IModuleController retVal = new DefaultModuleController(d);
 
 		// Replace the default with a configured instance, if one exists
-		CMExtension[] extensions = CMPlatformUtils
-					.getExtensions(ChoiceMakerExtensionPoint.CM_MODELMAKER_PLUGGABLECONTROLLER);
-		if (extensions == null || extensions.length < 1) {
-			throw new Error( "Invalid module: no extensions");
-		} else if (extensions.length > 1) {
-			throw new Error(
-				"Misconfigured module: " + extensions.length + " extensions");
-		} else {
-			assert extensions.length == 1 ;
-			CMExtension extension = extensions[0];
-			CMConfigurationElement[] els = extension.getConfigurationElements();
-			if (els == null || els.length != 1) {
-				int invalidLength = els == null ? 0 : els.length ;
-				throw new Error(
-					"Invalid module: " + invalidLength + " configurations");
-			}
-			CMConfigurationElement element = els[0];
-			try {
-				retVal =
-					(IModuleController) element.createExecutableExtension(
-						"class");
-				if (retVal instanceof IModelMakerAware) {
-					IModelMakerAware mma = (IModelMakerAware) retVal;
-					mma.setModelMakerObject(this.getModelMakerObject());
+		CMExtensionPoint pt =
+			CMPlatformUtils
+					.getPluginRegistry()
+					.getExtensionPoint(
+							ChoiceMakerExtensionPoint.CM_MODELMAKER_PLUGGABLECONTROLLER);
+		if (pt != null) {
+			CMExtension[] extensions = pt.getExtensions();
+			if (extensions.length > 1) {
+				throw new Error("too many extensions of IModule: "
+						+ extensions.length);
+			} else if (extensions.length == 1) {
+				CMExtension extension = extensions[0];
+				CMConfigurationElement[] els =
+					extension.getConfigurationElements();
+				if (els.length != 1) {
+					throw new Error(
+							"too many configurations of the default module: "
+									+ extensions.length);
 				}
-			} catch (Exception ex) {
-				ex.printStackTrace();
-				String msg =
-					"Error creating default configuration: "
-						+ ex.getMessage();
-				throw new Error(msg, ex);
+				CMConfigurationElement element = els[0];
+				try {
+					retVal =
+						(IModuleController) element
+								.createExecutableExtension("class");
+					if (retVal instanceof IModelMakerAware) {
+						IModelMakerAware mma = (IModelMakerAware) retVal;
+						mma.setModelMakerObject(this.getModelMakerObject());
+					}
+				} catch (Exception ex) {
+					ex.printStackTrace();
+					String msg =
+						"Error creating default configuration: "
+								+ ex.getMessage();
+					throw new Error(msg, ex);
+				}
 			}
 		}
 
