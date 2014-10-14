c9a06093376e35a3667f8c7f999c26d4cf4b432c
Rick Hall <rick@rphall.com>
2014-10-14 19:55:35 -0400

    Reformatted Java source code

--- 2.7.x/choicemaker-e2it/com.choicemaker.e2.ejb-it/src/test/java/com/choicemaker/e2it/ejb/EjbPlatformTest.java
+++ 2.7.x/choicemaker-e2it/com.choicemaker.e2.ejb-it/src/test/java/com/choicemaker/e2it/ejb/EjbPlatformTest.java
@@ -23,21 +23,18 @@ import com.choicemaker.e2it.PlatformTest;
 
 @RunWith(Arquillian.class)
 public class EjbPlatformTest {
-	
+
 	private static final String MAVEN_COORDINATE_SEPARATOR = ":";
-	
+
 	static final String PROJECT_POM = "pom.xml";
-	
+
 	static final String DEPENDENCIES_POM = PROJECT_POM;
-	
-	static final String EJB_MAVEN_GROUPID =
-		"com.choicemaker.e2";
 
-	static final String EJB_MAVEN_ARTIFACTID =
-			"com.choicemaker.e2.ejb";
+	static final String EJB_MAVEN_GROUPID = "com.choicemaker.e2";
 
-	static final String EJB_MAVEN_VERSION =
-			"2.7.1-SNAPSHOT";
+	static final String EJB_MAVEN_ARTIFACTID = "com.choicemaker.e2.ejb";
+
+	static final String EJB_MAVEN_VERSION = "2.7.1-SNAPSHOT";
 
 	static final String EJB_MAVEN_COORDINATES = new StringBuilder()
 			.append(EJB_MAVEN_GROUPID).append(MAVEN_COORDINATE_SEPARATOR)
@@ -51,8 +48,8 @@ public class EjbPlatformTest {
 		testClasses.add(PlatformTest.class);
 
 		JavaArchive ejb =
-				DeploymentUtils.createEjbJar(PROJECT_POM, EJB_MAVEN_COORDINATES,
-						testClasses, null);
+			DeploymentUtils.createEjbJar(PROJECT_POM, EJB_MAVEN_COORDINATES,
+					testClasses, null);
 
 		File[] deps = DeploymentUtils.createTestDependencies(DEPENDENCIES_POM);
 
@@ -72,10 +69,11 @@ public class EjbPlatformTest {
 	public void testInstallablePlatform() {
 		InstallablePlatform ip = InstallablePlatform.getInstance();
 		assertTrue(ip != null);
-		
-//		CMPlatform delegate = InstallablePlatform.getInstance().getDelegate();
-//		String delegateFQCN = delegate.getClass().getName();
-//		assertTrue(delegateFQCN.equals(EmbeddedPlatform.class.getName()));
+
+		// CMPlatform delegate =
+		// InstallablePlatform.getInstance().getDelegate();
+		// String delegateFQCN = delegate.getClass().getName();
+		// assertTrue(delegateFQCN.equals(EmbeddedPlatform.class.getName()));
 	}
 
 	@Test
