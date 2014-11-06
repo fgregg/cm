96291e4cee8bcb96ca9688b69a816721c3395708
Rick Hall <rick@rphall.com>
2014-11-06 15:58:00 -0500

    db.sqlserver: 2.7.1 versions of SqlServerParallelRecordSource and SQLServerOptimizedSerialRecordSource

---
+++ 2.7.x/choicemaker-cm/choicemaker-common/com.choicemaker.cm.io.db.sqlserver/src/main/java/com/choicemaker/cm/io/db/sqlserver/SQLServerOptimizedSerialRecordSource.java
@@ -0,0 +1,259 @@
+/*
+ * Created on Aug 18, 2004
+ *
+ */
+package com.choicemaker.cm.io.db.sqlserver;
+
+import java.io.IOException;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+import com.choicemaker.cm.core.ImmutableProbabilityModel;
+import com.choicemaker.cm.core.IncompleteSpecificationException;
+import com.choicemaker.cm.core.Record;
+import com.choicemaker.cm.core.Sink;
+import com.choicemaker.cm.core.base.AbstractRecordSourceSerializer;
+import com.choicemaker.cm.core.base.PMManager;
+import com.choicemaker.cm.io.db.base.ISerializableDbRecordSource;
+import com.choicemaker.util.Precondition;
+import com.choicemaker.util.StringUtils;
+
+/**
+ * This is a wrapper object around SqlServerParallelRecordSource and it can be
+ * serialized, because it stores string values with which to create the
+ * SqlServerRecordSource.
+ * 
+ * This is faster than SQLServerSerialRecordSource because it uses
+ * DbParallelReader instead of DbSerialReader.
+ * 
+ * @author pcheung
+ *
+ */
+public class SQLServerOptimizedSerialRecordSource implements ISerializableDbRecordSource {
+	
+	private static final long serialVersionUID = 271L;
+
+	private static final Logger log = Logger.getLogger(SQLServerOptimizedSerialRecordSource.class.getName());
+
+	private String dsJNDIName;
+	private String dsMapName;
+	private String modelName;
+	private String dbConfig;
+	private String sqlQuery;
+	
+	private transient SqlServerParallelRecordSource sqlRS;
+	private transient DataSource ds;
+	private transient ImmutableProbabilityModel model;
+
+	public SQLServerOptimizedSerialRecordSource(String dsJNDIName,
+			String dsMapName, String modelName, String dbConfig,
+			String sqlQuery, int maxCompositeSize) {
+
+		this.dsJNDIName = dsJNDIName;
+		this.dsMapName = dsMapName;
+		this.modelName = modelName;
+		this.dbConfig = dbConfig;
+		this.sqlQuery = sqlQuery;
+	}
+
+	private DataSource getDataSource () {
+		try {
+			if (ds == null) {
+				Context ctx = new InitialContext();
+				ds = (DataSource) ctx.lookup (dsJNDIName);
+			}
+		} catch (NamingException ex) {
+			log.severe(ex.toString());
+		}
+		return ds;
+	}
+	
+	private SqlServerParallelRecordSource getRS () {
+		if (sqlRS == null) {
+			sqlRS = new SqlServerParallelRecordSource ("", getModel(),
+				dsJNDIName, dbConfig, getSqlQuery());
+			sqlRS.setDataSource(dsJNDIName, getDataSource());
+
+		}
+		return sqlRS;
+	}
+	
+	public ImmutableProbabilityModel getModel () {
+		if (model == null) {
+			model = PMManager.getModelInstance(modelName);
+		}
+		return model;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.choicemaker.cm.core.RecordSource#getNext()
+	 */
+	public Record getNext() throws IOException {
+		return getRS().getNext();
+	}
+
+	/* (non-Javadoc)
+	 * @see com.choicemaker.cm.core.Source#open()
+	 */
+	public void open() throws IOException {
+		getRS().open ();
+	}
+
+	/* (non-Javadoc)
+	 * @see com.choicemaker.cm.core.Source#close()
+	 */
+	public void close() throws IOException {
+		getRS().close();
+	}
+
+	/* (non-Javadoc)
+	 * @see com.choicemaker.cm.core.Source#hasNext()
+	 */
+	public boolean hasNext() throws IOException {
+		return getRS().hasNext();
+	}
+
+	/* (non-Javadoc)
+	 * @see com.choicemaker.cm.core.Source#getName()
+	 */
+	public String getName() {
+		return getRS().getName();
+	}
+
+	/* (non-Javadoc)
+	 * @see com.choicemaker.cm.core.Source#setName(java.lang.String)
+	 */
+	public void setName(String name) {
+		getRS().setName(name);
+	}
+
+	/* (non-Javadoc)
+	 * @see com.choicemaker.cm.core.Source#setModel(com.choicemaker.cm.core.ImmutableProbabilityModel)
+	 */
+	public void setModel(ImmutableProbabilityModel m) {
+		getRS().setModel(m);
+	}
+
+	/* (non-Javadoc)
+	 * @see com.choicemaker.cm.core.Source#hasSink()
+	 */
+	public boolean hasSink() {
+		return getRS().hasSink();
+	}
+
+	/* (non-Javadoc)
+	 * @see com.choicemaker.cm.core.Source#getSink()
+	 */
+	public Sink getSink() {
+		return getRS().getSink();
+	}
+
+	/* (non-Javadoc)
+	 * @see com.choicemaker.cm.core.Source#getFileName()
+	 */
+	public String getFileName() {
+		return getRS().getFileName();
+	}
+
+	public boolean equals (Object o) {
+		if (o instanceof SQLServerOptimizedSerialRecordSource) {
+			SQLServerOptimizedSerialRecordSource rs = (SQLServerOptimizedSerialRecordSource) o;
+			return rs.dbConfig.equals(this.dbConfig) && 
+				rs.dsJNDIName.equals(this.dsJNDIName) &&
+				rs.modelName.equals(this.modelName) &&
+				rs.getSqlQuery().equals(this.getSqlQuery()) &&
+				rs.dsMapName.equals(this.dsMapName);
+		} else {
+			return false;
+		}
+	}
+	
+	public int hashCode () {
+		return getSqlQuery().hashCode();
+	}
+
+	public String getDsJNDIName() {
+		return dsJNDIName;
+	}
+
+	public String getModelName() {
+		return modelName;
+	}
+
+	public String getDbConfig() {
+		return dbConfig;
+	}
+
+	public String getSqlQuery() {
+		return sqlQuery;
+	}
+
+	public Properties getProperties() {
+		Properties retVal = new Properties();
+		retVal.setProperty(PN_DATASOURCE_JNDI_NAME, this.getDsJNDIName());
+		retVal.setProperty(PN_MODEL_NAME, this.getModelName());
+		retVal.setProperty(PN_DATABASE_CONFIG, this.getDbConfig());
+		retVal.setProperty(PN_SQL_QUERY, this.getSqlQuery());
+		return retVal;
+	}
+
+	public void  setProperties(Properties properties) throws IncompleteSpecificationException {
+
+		Precondition.assertNonNullArgument("null properties",properties);
+
+		String s = properties.getProperty(PN_DATABASE_CONFIG);
+		if (!StringUtils.nonEmptyString(s)) {
+			String msg = "Missing property '" + PN_DATABASE_CONFIG + "'";
+			log.severe(msg);
+			throw new IncompleteSpecificationException(msg);
+		}
+		this.dbConfig = s;
+
+		s = properties.getProperty(PN_DATASOURCE_JNDI_NAME);
+		if (!StringUtils.nonEmptyString(s)) {
+			String msg = "Missing property '" + PN_DATASOURCE_JNDI_NAME + "'";
+			log.severe(msg);
+			throw new IncompleteSpecificationException(msg);
+		}
+		this.dsJNDIName = s;
+
+		s = properties.getProperty(PN_MODEL_NAME);
+		if (!StringUtils.nonEmptyString(s)) {
+			String msg = "Missing property '" + PN_MODEL_NAME + "'";
+			log.severe(msg);
+			throw new IncompleteSpecificationException(msg);
+		}
+		this.modelName = s;
+
+		s = properties.getProperty(PN_SQL_QUERY);
+		if (!StringUtils.nonEmptyString(s)) {
+			String msg = "Missing property '" + PN_SQL_QUERY + "'";
+			log.severe(msg);
+			throw new IncompleteSpecificationException(msg);
+		}
+		this.sqlQuery = s;
+
+		// Reset the underlaying record source (it will be lazily recreated)
+		try {
+			if (sqlRS != null) {
+				sqlRS.close();
+			}
+		} catch (Exception x) {
+			String msg = "Unable to close " + (sqlRS == null ? "record source" : sqlRS.getName());
+			log.warning(msg);
+		} finally {
+			sqlRS = null;
+		}
+	}
+
+	public String toXML() {
+		String retVal = AbstractRecordSourceSerializer.toXML(this);
+		return retVal;
+	}
+
+}
---
+++ 2.7.x/choicemaker-cm/choicemaker-common/com.choicemaker.cm.io.db.sqlserver/src/main/java/com/choicemaker/cm/io/db/sqlserver/SqlServerParallelRecordSource.java
@@ -0,0 +1,296 @@
+/*
+ * Created on Feb 5, 2004
+ *
+ * To change the template for this generated file go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+package com.choicemaker.cm.io.db.sqlserver;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Logger;
+
+import javax.sql.DataSource;
+
+import com.choicemaker.cm.core.Accessor;
+import com.choicemaker.cm.core.ImmutableProbabilityModel;
+import com.choicemaker.cm.core.Record;
+import com.choicemaker.cm.core.RecordSource;
+import com.choicemaker.cm.core.Sink;
+import com.choicemaker.cm.io.db.base.DataSources;
+import com.choicemaker.cm.io.db.base.DbAccessor;
+import com.choicemaker.cm.io.db.base.DbReaderParallel;
+import com.choicemaker.cm.io.db.base.DbView;
+
+/**
+ * @author pcheung
+ */
+public class SqlServerParallelRecordSource implements RecordSource {
+
+	private static Logger logger = Logger.getLogger(SqlServerParallelRecordSource.class.getName());
+
+
+	private String fileName;
+
+	private ImmutableProbabilityModel model;
+	private String dbConfiguration;
+	private String idsQuery;
+
+	private String dsName;
+	private DataSource ds;
+	private Connection connection;
+	
+	private DbReaderParallel dbr;
+	private Statement [] selects;
+	private ResultSet [] results;
+	
+	private static final String DATA_VIEW = "DATAVIEW_1001";
+	
+	public SqlServerParallelRecordSource() {
+		// do nothing...
+	}
+	
+	public SqlServerParallelRecordSource(String fileName, ImmutableProbabilityModel model, String dsName, String dbConfiguration, String idsQuery) {
+		
+		logger.fine("Constructor: " + fileName + " " + model + " " + dsName + " " + dbConfiguration + " " + idsQuery);
+		
+		this.model = model;
+		setDataSourceName(dsName);
+		this.dbConfiguration = dbConfiguration;
+		this.idsQuery = idsQuery;
+	}
+
+	public void open() throws IOException {
+		
+		if (!isValidQuery()) throw new IOException ("idsQuery must contain ' AS ID '.");
+		
+		DbAccessor accessor = (DbAccessor) model.getAccessor();
+		dbr = accessor.getDbReaderParallel(dbConfiguration);
+		
+		try {
+			if (connection == null) {
+				connection = ds.getConnection();
+				connection.setAutoCommit(true);
+			}
+			//connection.setReadOnly(true);
+			
+			//1.	Create view
+			createView (connection);
+			
+			//2.	Get the ResultSets
+			getResultSets ();
+			
+			//3.	Open parallel reader
+			logger.fine("before dbr.open");
+			dbr.open(results);
+		} catch (SQLException ex) {
+			logger.severe(ex.toString());
+			
+			throw new IOException(ex.toString(), ex);
+		}
+	}
+	
+	
+	private void createView (Connection conn) throws SQLException {
+		Statement view = conn.createStatement();
+		String s = "IF EXISTS (SELECT TABLE_NAME FROM INFORMATION_SCHEMA.VIEWS WHERE TABLE_NAME = '" + DATA_VIEW +
+		"') DROP VIEW " + DATA_VIEW;
+		logger.fine(s);
+		view.execute(s);
+		
+		s = "create view " + DATA_VIEW + " as " + idsQuery;
+		logger.fine(s);
+		view.execute(s);
+		view.close();
+	}
+	
+	private void dropView (Connection conn) throws SQLException {
+		Statement view = conn.createStatement();
+		view.execute("IF EXISTS (SELECT TABLE_NAME FROM INFORMATION_SCHEMA.VIEWS WHERE TABLE_NAME = '" + DATA_VIEW +
+		 "') DROP VIEW " + DATA_VIEW);
+		view.close();
+	}
+	
+
+	
+	/** This method checks to make sure the idsQuery is valid.  It must contain "as id".
+	 * For example, "select distinct TAP_CORPORATE_ID as ID from CORPORATE where primary_name like 'A%'"
+	 * 
+	 * @return
+	 */
+	private boolean isValidQuery () {
+		if (idsQuery.toUpperCase().indexOf(" AS ID ") == -1) return false;
+		else return true;
+	}
+	
+	
+	
+	private void getResultSets () throws SQLException {
+		Accessor accessor = model.getAccessor();
+		String viewBase = "vw_cmt_" + accessor.getSchemaName() + "_r_" + dbConfiguration;
+		DbView[] views = dbr.getViews();
+		String masterId = dbr.getMasterId();
+		
+		int numViews = views.length;
+		selects = new Statement [numViews];
+		results = new ResultSet [numViews];
+
+		for (int i = 0; i < numViews; ++i) {
+			String viewName = viewBase + i;
+			logger.finest("view: " + viewName);
+			DbView v = views[i];
+
+			StringBuffer sb = new StringBuffer ("select * from "); 
+			sb.append(viewBase);
+			sb.append(i);
+			sb.append(" where ");
+			sb.append(masterId);
+			sb.append(" in (select id from ");
+			sb.append(DATA_VIEW);
+			sb.append(")");
+			
+			if (v.orderBy.length > 0) {
+				sb.append(" ORDER BY ");
+				sb.append(getOrderBy(v));
+			}
+			
+			String queryString = sb.toString();
+			
+			logger.fine ("Query: " + queryString);
+			
+			selects[i] = connection.prepareStatement(queryString);
+			logger.fine("Prepared statement");
+			selects[i].setFetchSize(100);
+			logger.fine("Changed Fetch Size to 100");
+			results[i] = ((PreparedStatement) selects[i]).executeQuery();
+			logger.fine("Executed query " + i);		
+		}		
+	}
+	
+	
+	private static String getOrderBy(DbView v) {
+		StringBuffer ob = new StringBuffer();
+		for (int j = 0; j < v.orderBy.length; ++j) {
+			if (j != 0)
+				ob.append(",");
+			ob.append(v.orderBy[j].name);
+		}
+		return ob.toString();
+	}
+	
+
+	public boolean hasNext() throws IOException {
+		return dbr.hasNext();
+	}
+
+	public Record getNext() throws IOException {
+		Record r = null;
+		try {
+			r = dbr.getNext();
+		} catch (SQLException e) {
+			throw new IOException (e.toString());
+		}
+		return r;
+	}
+
+
+	public void close() throws IOException {
+		try {
+			int s = selects.length;
+			for (int i=0; i<s; i++) {
+				selects[i].close();
+				results[i].close();
+			}
+			
+			
+			dropView (connection);
+			
+			if (ds != null) {
+				connection.close();
+				connection = null;
+			}
+		} catch (SQLException ex) {
+			throw new IOException("Problem closing the statement or connection.", ex);
+		} finally {
+			dbr = null;
+		}
+	}
+
+	public ImmutableProbabilityModel getModel() {
+		return model;
+	}
+
+	public void setModel(ImmutableProbabilityModel m) {
+		this.model = m;
+	}
+	
+	public String getDataSourceName() {
+		return dsName;
+	}
+
+	public void setDataSourceName(String dsName) {
+		DataSource ds = DataSources.getDataSource(dsName);
+		setDataSource(dsName, ds);
+	}
+		
+	public DataSource getDataSource() {
+		return ds;
+	}
+	
+	public void setDataSource(String name, DataSource ds) {
+		this.dsName = name;
+		this.ds = ds;
+	}
+	
+	public void setConnection(Connection connection) {
+		this.dsName = null;
+		this.ds = null;
+		
+		this.connection = connection;
+	}
+	
+	public String getDbConfiguration() {
+		return dbConfiguration;
+	}
+	
+	public void setDbConfiguration(String dbConfiguration) {
+		this.dbConfiguration = dbConfiguration;
+	}
+	
+	public String getIdsQuery() {
+		return idsQuery;
+	}
+	
+	public void setIdsQuery(String idsQuery) {
+		this.idsQuery = idsQuery;
+	}
+	
+	public String getName() {
+		return "SQL Server Parallel Record Source";
+	}
+
+	public void setName(String name) {
+		throw new UnsupportedOperationException();
+	}
+
+	public boolean hasSink() {
+		return false;
+	}
+
+	public Sink getSink() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+}
