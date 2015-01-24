d95e3a777c83a79683de00f2b1ec1342d7c7581d
Rick Hall <rick@rphall.com>
2015-01-24 15:17:06 -0500

    oaba: Added verbose debugging to ChunkDataStore

--- 2.7.x/choicemaker-cm/choicemaker-j2ee/com.choicemaker.cm.io.blocking.automated.offline.server/src/main/java/com/choicemaker/cm/io/blocking/automated/offline/server/data/ChunkDataStore.java
+++ 2.7.x/choicemaker-cm/choicemaker-j2ee/com.choicemaker.cm.io.blocking.automated.offline.server/src/main/java/com/choicemaker/cm/io/blocking/automated/offline/server/data/ChunkDataStore.java
@@ -13,6 +13,8 @@ package com.choicemaker.cm.io.blocking.automated.offline.server.data;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import com.choicemaker.cm.core.BlockingException;
 import com.choicemaker.cm.core.IControl;
@@ -30,11 +32,17 @@ import com.choicemaker.cm.io.blocking.automated.offline.utils.ControlChecker;
  */
 public class ChunkDataStore {
 
+	private static final Logger logger = Logger.getLogger(ChunkDataStore.class
+			.getName());
+
+	// public static final int DEFAULT_LOOP_CONTROL = 10000;
+	public static final int DEFAULT_LOOP_CONTROL = 100;
+
 	// Make this explicitly not serializable
 	private transient Map<Object, Record> stageMap = null;
 	private transient Map<Object, Record> masterMap = null;
 
-	private static ChunkDataStore dataStore = null;
+	private static ChunkDataStore dataStore = new ChunkDataStore();
 
 	private ChunkDataStore() {
 	}
@@ -43,8 +51,6 @@ public class ChunkDataStore {
 	 * This gets the single instance object.
 	 */
 	public static ChunkDataStore getInstance() {
-		if (dataStore == null)
-			dataStore = new ChunkDataStore();
 		return dataStore;
 	}
 
@@ -64,36 +70,58 @@ public class ChunkDataStore {
 	 * @param control
 	 *            - control object
 	 */
-	public void init(RecordSource stageSource, ImmutableProbabilityModel model,
-			RecordSource masterSource, int maxChunkSize, IControl control)
-			throws BlockingException {
+	public synchronized void init(RecordSource stageSource,
+			ImmutableProbabilityModel model, RecordSource masterSource,
+			int maxChunkSize, IControl control) throws BlockingException {
 
-		stageMap = readMap(stageSource, model, maxChunkSize + 10, control);
-		masterMap = readMap(masterSource, model, maxChunkSize + 10, control);
+		final String s = "Staging";
+		stageMap = readMap(stageSource, model, maxChunkSize + 10, control, s);
+		logger.info("Stage map size: " + stageMap.size());
+
+		final String m = "Master";
+		masterMap = readMap(masterSource, model, maxChunkSize + 10, control, m);
+		logger.info("Master map size: " + masterMap.size());
 	}
 
 	private static Map<Object, Record> readMap(RecordSource rs,
-			ImmutableProbabilityModel model, int maxSize, IControl control)
-			throws BlockingException {
+			ImmutableProbabilityModel model, int maxSize, IControl control,
+			String tag) throws BlockingException {
 
 		Map<Object, Record> dataMap = new HashMap<>(maxSize, 1.0f);
 		try {
-			if (rs != null && model != null) {
+			if (rs == null) {
+				logger.info(tag + ": null record source -- skipping map");
+			} else if (model == null) {
+				logger.info(tag + ": null model -- skipping map");
+			} else {
+				logger.fine(tag + ": creating map");
+				assert rs != null && model != null;
 				rs.setModel(model);
+				logger.fine(tag + ": opening record source");
 				rs.open();
 
+				logger.fine(tag + ": checking job control");
 				boolean stop = control.shouldStop();
-				int c = 0;
 
-				// put the whole chunk dataset into memory.
+				// put the whole chunk data set into memory.
+				int c = 0;
 				while (rs.hasNext() && !stop) {
 					Record r = rs.getNext();
 					Object O = r.getId();
 					dataMap.put(O, r);
-					ControlChecker.checkStop(control, ++c);
+					++c;
+					if (c % DEFAULT_LOOP_CONTROL == 1) {
+						logger.fine(tag + ": record count: " + c);
+						logger.fine(tag + ": record id: " + O + " (class "
+								+ (O == null ? null : O.getClass().getName())
+								+ ")");
+					}
+					ControlChecker.checkStop(control, c);
 				}
+				logger.fine(tag + ": total record count: " + c);
 
 				rs.close();
+				logger.fine(tag + ": finished map");
 			}
 		} catch (IOException ex) {
 			throw new BlockingException(ex.toString());
@@ -107,7 +135,7 @@ public class ChunkDataStore {
 	 *
 	 *
 	 */
-	public void cleanUp() {
+	public synchronized void cleanUp() {
 		stageMap = null;
 		masterMap = null;
 	}
@@ -116,14 +144,47 @@ public class ChunkDataStore {
 	 * This method returns the staging record with the given id.
 	 */
 	public Object getStage(Object id) {
-		return stageMap.get(id);
+		if (stageMap == null) {
+			String msg = "null stage map";
+			logger.severe(msg);
+			throw new IllegalStateException(msg);
+		}
+		if (logger.isLoggable(Level.FINE) && stageMap.size() == 0) {
+			// Issue warnings only for fine-grained logging
+			logger.warning("stageMap is empty");
+		}
+		Object retVal = stageMap.get(id);
+		if (logger.isLoggable(Level.FINE) && retVal == null) {
+			// Issue warnings only for fine-grained logging
+			logger.warning("null staging record (id " + id + ", stageMap size "
+					+ stageMap.size() + ", class "
+					+ (id == null ? null : id.getClass().getName()) + ")");
+		}
+		return retVal;
 	}
 
 	/**
 	 * This method returns the master record with the given id.
 	 */
 	public Object getMaster(Object id) {
-		return masterMap.get(id);
+		if (masterMap == null) {
+			String msg = "null master map";
+			logger.severe(msg);
+			throw new IllegalStateException(msg);
+		}
+		if (logger.isLoggable(Level.FINE) && masterMap.size() == 0) {
+			// Issue warnings only for fine-grained logging
+			logger.warning("masterMap is empty");
+		}
+		Object retVal = masterMap.get(id);
+		if (logger.isLoggable(Level.FINE) && retVal == null) {
+			// Issue warnings only for fine-grained logging
+			logger.warning("null master record (id " + id + ")");
+			logger.warning("null master record (id " + id + ", masterMap size "
+					+ masterMap.size() + ", class "
+					+ (id == null ? null : id.getClass().getName()) + ")");
+		}
+		return retVal;
 	}
 
 }
