6e90f3c33957494a60718cb76643c87af648e437
Rick Hall <rick@rphall.com>
2015-01-30 15:36:10 -0500

    oaba: Extracted an interface for OabaParametersControllerBean

---
+++ 2.7.x/choicemaker-cm/choicemaker-j2ee/com.choicemaker.cm.io.blocking.automated.offline.server/src/main/java/com/choicemaker/cm/io/blocking/automated/offline/server/impl/OabaParametersController.java
@@ -0,0 +1,24 @@
+package com.choicemaker.cm.io.blocking.automated.offline.server.impl;
+
+import java.util.List;
+
+import javax.ejb.Local;
+
+import com.choicemaker.cm.args.OabaParameters;
+
+@Local
+public interface OabaParametersController {
+
+	void delete(OabaParameters p);
+
+	void detach(OabaParameters p);
+
+	List<OabaParameters> findAllOabaParameters();
+
+	OabaParameters findOabaParameters(long id);
+
+	OabaParameters findOabaParametersByJobId(long jobId);
+
+	OabaParameters save(OabaParameters p);
+
+}
