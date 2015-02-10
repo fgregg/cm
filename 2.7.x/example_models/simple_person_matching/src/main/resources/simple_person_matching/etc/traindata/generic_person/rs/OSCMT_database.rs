b065a44439517ea691b9c8c548f96b5bb036d7ad
Rick Hall <rick@rphall.com>
2015-02-10 07:20:49 -0800

    example_models: Added DB record source descriptors appropriate for MS SQL Server

--- 2.7.x/example_models/simple_person_matching/src/main/resources/simple_person_matching/etc/traindata/generic_person/rs/OSCMT_database.rs
+++ 2.7.x/example_models/simple_person_matching/src/main/resources/simple_person_matching/etc/traindata/generic_person/rs/OSCMT_database.rs
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <RecordSource class="com.choicemaker.cm.io.db.base.dbRsReader" conf="default">
-    <selection>select record_id "ID" from PERSON</selection>
+    <selection>select record_id ID from PERSON</selection>
     <connectionName>persons_1</connectionName>
 </RecordSource>
---
+++ 2.7.x/example_models/simple_person_matching/src/main/resources/simple_person_matching/etc/traindata/generic_person/rs/master-dbquery.rs
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<RecordSource class="com.choicemaker.cm.io.db.sqlserver.sqlServerRsReader" parallel="true" dataSourceName="localhost" dbConfiguration="default">
+    <idsQuery>select record_id as ID from PERSON where linkage_role = 'M'</idsQuery>
+</RecordSource>
