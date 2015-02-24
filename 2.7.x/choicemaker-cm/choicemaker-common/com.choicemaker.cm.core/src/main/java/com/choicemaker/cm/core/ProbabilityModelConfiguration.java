8a7feb0e560edf8187ce8931e36cf6e2a3b656a1
Rick Hall <rphall@choicemaker.com>
2015-02-24 13:31:39 -0500

    cm-core: Added deprecated property names to ProbabilityModelConfiguration

--- 2.7.x/choicemaker-cm/choicemaker-common/com.choicemaker.cm.core/src/main/java/com/choicemaker/cm/core/ProbabilityModelConfiguration.java
+++ 2.7.x/choicemaker-cm/choicemaker-common/com.choicemaker.cm.core/src/main/java/com/choicemaker/cm/core/ProbabilityModelConfiguration.java
@@ -12,8 +12,24 @@ public interface ProbabilityModelConfiguration {
 	 * Name of a required plugin attribute that specifies a named database configuration
 	 * in the record-layout schema of the model.
 	 */
-
 	String AN_DATABASE_CONFIGURATION = "databaseConfiguration";
+
+	// Deprecated property names
+
+	/** @deprecated */
+	String PN_BLOCKING_CONFIGURATION = AN_BLOCKING_CONFIGURATION;
+
+	/** @deprecated */
+	String PN_DB_CONFIGURATION = "dbConfiguration";
+
+	/** @deprecated */
+	String PN_LIMITPERBLOCKINGSET = "limitPerBlockingSet";
+
+	/** @deprecated */
+	String PN_SINGLETABLEBLOCKINGSETGRACELIMIT = "limitSingleBlockingSet";
+
+	/** @deprecated */
+	String PN_LIMITSINGLEBLOCKINGSET = "limitPerBlockingSet";
 
 	ImmutableProbabilityModel getProbabilityModel();
 	
