180ace16252bacdab42e73c49b1605f72c04faf6
Rick Hall <rphall@choicemaker.com>
2015-03-20 12:42:00 -0400

    cmit: Initial version of IT for Single Record Mode of the OabaService

---
+++ 2.7.x/choicemaker-cmit/cm-server-oaba-it/src/test/java/com/choicemaker/cmit/oaba/srm/SrmOabaServiceBeanIT.java
@@ -0,0 +1,62 @@
+package com.choicemaker.cmit.oaba.srm;
+
+import static com.choicemaker.cm.args.BatchProcessing.EVT_INIT;
+import static com.choicemaker.cm.args.BatchProcessing.PCT_INIT;
+
+import java.util.logging.Logger;
+
+import javax.jms.Queue;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.spec.EnterpriseArchive;
+import org.junit.runner.RunWith;
+
+import com.choicemaker.cm.batch.BatchJob;
+import com.choicemaker.cmit.oaba.AbstractOabaMdbTest;
+import com.choicemaker.cmit.oaba.util.OabaDeploymentUtils;
+import com.choicemaker.cmit.testconfigs.SimplePersonSqlServerSRMConfiguration;
+import com.choicemaker.cmit.utils.BatchProcessingPhase;
+
+@RunWith(Arquillian.class)
+public class SrmOabaServiceBeanIT extends
+		AbstractOabaMdbTest<SimplePersonSqlServerSRMConfiguration> {
+
+	private static final Logger logger = Logger
+			.getLogger(SrmOabaServiceBeanIT.class.getName());
+
+	private static final boolean TESTS_AS_EJB_MODULE = false;
+
+	private final static String LOG_SOURCE = SrmOabaServiceBeanIT.class
+			.getSimpleName();
+
+	/**
+	 * Creates an EAR deployment in which the OABA server JAR is missing the
+	 * StartOabaMDB message bean. This allows another class to attach to the
+	 * startQueue for testing.
+	 */
+	@Deployment
+	public static EnterpriseArchive createEarArchive() {
+		Class<?>[] removedClasses = { /* StartOabaMDB.class */ };
+		return OabaDeploymentUtils.createEarArchive(removedClasses,
+				TESTS_AS_EJB_MODULE);
+	}
+
+	public SrmOabaServiceBeanIT() {
+		super(LOG_SOURCE, logger, EVT_INIT, PCT_INIT,
+				SimplePersonSqlServerSRMConfiguration.class,
+				BatchProcessingPhase.INITIAL);
+	}
+
+	@Override
+	public final Queue getResultQueue() {
+		return getStartQueue();
+	}
+
+	/** Stubbed implementation that does not check the working directory */
+	@Override
+	public boolean isWorkingDirectoryCorrectAfterProcessing(BatchJob batchJob) {
+		return true;
+	}
+
+}
