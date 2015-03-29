8d1ce86c9b55c3a0ca95193cc76c6295c9e2fb93
Rick Hall <rphall@choicemaker.com>
2015-03-28 21:22:51 -0400

    deprecated: PersistentAbaStatistics.java

--- 2.7.x/choicemaker-cm/choicemaker-common/com.choicemaker.cm.io.blocking.automated.base/src/main/java/com/choicemaker/cm/io/blocking/automated/AbaStatistics.java
+++ 2.7.x/choicemaker-cm/choicemaker-common/com.choicemaker.cm.io.blocking.automated.base/src/main/java/com/choicemaker/cm/io/blocking/automated/AbaStatistics.java
@@ -10,53 +10,13 @@
  */
 package com.choicemaker.cm.io.blocking.automated;
 
-import java.io.Serializable;
-
-import com.choicemaker.cm.core.ImmutableProbabilityModel;
+import java.io.IOException;
 
 /**
- * Statistics for the online/real-time ABA (Automated Batch Algorithm) are
- * matching parameters that change relatively infrequently. Prior to version 2.7
- * of ChoiceMaker, these statistics were stored with each instance of a model
- * configuration. However, since statistics depend only on the schema of a
- * model, and since many model configurations can share the same schema, it
- * makes more sense to store ABA statistics in an object that is separate from
- * any particular model. In a J2EE environment, ABA statistics are cached in
- * memory by an EJB singleton service.
- * 
- * @see com.choicemaker.cm.io.blocking.automated.base.db.DefaultAbaStatisticsBean
- * 
- * @author rphall
- *
+ * Computes and sets the number of occurrences ('counts') of blocking values
+ * for a particular blocking configuration.
  */
-public interface AbaStatistics extends Serializable {
-
-	/** Default id value for non-persistent settings */
-	long NONPERSISTENT_ABA_STATISTICS_ID = 0;
-
-	/**
-	 * The persistence identifier for an instance. If the value is
-	 * {@link #NONPERSISTENT_ABA_STATISTICS_ID}, then the settings are not
-	 * persistent.
-	 */
-	long getId();
-
-	/**
-	 * Statistics for the online Automated Blocking Algorithm. ABA statistics
-	 * are specific to a particular database and blocking configuration of a
-	 * given model
-	 * 
-	 * @param model
-	 *            A configured model (loaded as a plugin)
-	 * @param databaseConfigurationName
-	 *            the name of a database configuration specified by the model
-	 *            schema
-	 * @param blockingConfigurationName
-	 *            the name of a blocking configuration specified by the model
-	 *            schema
-	 * @return
-	 */
-	CountSource getCountSource(ImmutableProbabilityModel model,
-			String databaseConfigurationName, String blockingConfigurationName);
-
+public interface AbaStatistics {
+	long computeBlockingValueCounts(IBlockingConfiguration configuration,
+			IBlockingValue[] blockingValues) throws IOException;
 }
